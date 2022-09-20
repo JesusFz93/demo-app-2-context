b1eeb9d98f8f6cab6be1051402590669b32b3834
Jesus <jesus.fdz93@gmail.com>
2022-09-19 20:35:49 -0500

    paypal

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ const App = () => {
     <AuthProvider>
       <PayPalScriptProvider
         options={{
-          "client-id": "",
+          "client-id":
+            "AVxE4OsPYQFvrnhcDPeu1x78puMjbmXWKJM4bXqIJF3t86N46keduJHroaUFzly_eVB5Vd9X9zBI1p4n",
         }}
       >
         <ProductState>
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext, useEffect } from "react";
 import ProductContext from "../context/ProductContext";
 import CartList from "../components/CartList";
 import Title from "../components/Title";
+import PaypalCheckoutButton from "../components/PaypalCheckoutButton";
 
 const CartPage = () => {
   const { cart } = useContext(ProductContext);
@@ -19,11 +20,31 @@ const CartPage = () => {
           <CartList />
         </article>
       </main>
-      <section className="row pt-5">
-        <article className="col-md-12">
-          <h2>{total}</h2>
-        </article>
-      </section>
+
+      {cart.length > 0 ? (
+        <>
+          <section className="row pt-5">
+            <article className="col-md-12">
+              <h2>{total}</h2>
+            </article>
+          </section>
+          <section className="row">
+            <article className="col">
+              <PaypalCheckoutButton
+                currency={"MXN"}
+                amount={total}
+                showSpinner={false}
+              />
+            </article>
+          </section>
+        </>
+      ) : (
+        <section className="row">
+          <article className="col">
+            <h2>No hay productos en el carrito</h2>
+          </article>
+        </section>
+      )}
     </>
   );
 };
