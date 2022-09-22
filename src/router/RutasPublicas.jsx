e80b62f938469922d53dec744366cc444bbd70f4
Jesus <jesus.fdz93@gmail.com>
2022-09-22 18:45:41 -0500

    rutas publicas y privadas

--- src/router/RutasPublicas.jsx
+++ src/router/RutasPublicas.jsx
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import NavbarPublico from "../components/NavbarPublico";
 import HomePage from "../pages/HomePage";
